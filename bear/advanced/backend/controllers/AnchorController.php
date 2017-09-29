6d209adb0da1b5b8b59453d3a01950fccb609e07
ymm <1052418826@qq.com>
2017-09-29 09:10:45 +0800

    ymm接口
    接口

--- bear/advanced/backend/controllers/AnchorController.php
+++ bear/advanced/backend/controllers/AnchorController.php
@@ -5,26 +5,32 @@ use yii\web\Controller;
 class AnchorController extends Controller
 {
 	//展示所有的主播
-	public function show()
+	public function actionShow()
 	{
 		$callback=$_GET['callback'];
 		$db=Yii::$app->db;
-		$res=$db->createCommand("select * from anchor where status=0")->queryAll();		
-		echo $callback."(".json_encode($res).")";
+		$res=$db->createCommand("select * from anchor where status=0")->queryAll();	
+		$msg['error']=1;
+		$msg['data']=$res;	
+		echo $callback."(".json_encode($msg).")";
 	}
 	//修改主播状态
-	public function update()
+	public function actionUpdate()
 	{
 		$callback=$_GET['callback'];
 		$db=Yii::$app->db;
 		$status=$_GET['status'];
-		$id=$_GET['id'];
+		$id=$_GET['anchor_id'];
 		$db=Yii::$app->db;
 		$res=$db->createCommand()->update("update anchor set status='$status' where id=$id")->execute();
 		if ($res) {
-			$msg['error']=1;
+			$time=time();
+			$a=$db->createCommand()->insert("insert into room value('','','$id','$time')")->execute();
+			if ($a) {
+				$msg['error']=1;
+			}
 		}else{
-			$msg['error']=0;
+				$msg['error']=0;
 		}
 		echo $callback."(".json_encode($msg).")";
 	}
--- bear/advanced/backend/controllers/GiftController.php
+++ bear/advanced/backend/controllers/GiftController.php
@@ -5,7 +5,7 @@ use yii\web\Controller;
 class GiftController extends Controller
 {
 	//添加礼物
-	public function add()
+	public function actionAdd()
 	{
 		$money=$_GET['money'];
 		$name=$_GET['name'];
@@ -20,13 +20,55 @@ class GiftController extends Controller
 		echo $callback."(".json_encode($msg).")";
 	}
 	//展示礼物
-	public function show()
+	public function actionShow()
 	{
 		$callback=$_GET['callback'];
-		$res=$db->createCommand("select * from gift")->queryAll();
-		$msg['gift']=$res;
+		$db=Yii::$app->db;
+		if ($_GET['id']) {
+			$id=$_GET['id'];
+			$res=$db->createCommand("select * from gift where id=$id")->queryOne();
+		}
+			$res=$db->createCommand("select * from gift")->queryAll();
+		if ($res) {
+			$msg['data']=$res;
+			$msg['error']=1;
+		}else{
+			$msg['error']=0;
+		}
 		echo $callback."(".json_encode($msg).")";
 	}
+
+	public function actionDel()
+	{
+		$callback=$_GET['callback'];
+		$id=$_GET['id'];
+		$db=Yii::$app->db;
+		$res=$db->createCommand("delete from gift where id=$id")->execute();
+		if ($res) {
+			$msg['error']=1;
+		}else{
+			$msg['error']=0;
+		}
+		echo $callback."(".json_encode($msg).")";
+
+	}
+
+	public function actionUpdate()
+	{
+		$callback=$_GET['callback'];
+		$id=$_GET['id'];
+		$name=$_GET['name'];
+		$money=$_GET['money'];
+		$db=Yii::$app->db;
+		$res=$db->createCommand()->update("update gift set name='$name',money='$money' where id=$id")->execute();
+		if ($res) {
+			$msg['error']=1;
+		}else{
+			$msg['error']=0;
+		}
+		echo $callback."(".json_encode($msg).")";
+	}
+
 }
 
 
