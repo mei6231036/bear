9bd6e433ec345217049527fa0c6bff6447fca6c7
zyk19971107 <897696250@qq.com>
2017-10-08 11:26:39 +0800

    登录注册
    登录注册的添加

---
+++ bear/laravel52/app/Http/Controllers/Login/LoginController.php
@@ -0,0 +1,70 @@
+<?php
+namespace App\Http\Controllers\Login;
+use DB;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LoginController extends Controller
+{
+	//注册
+	public function regin(Request $request)
+	{
+		$myip =$this-> get_client_ip();//获取ip
+		if($request->isMethod('get')){
+	        $data['email'] 	  =	$request->input('email');
+	        $first = DB::table('user')->select(['email'])->where('email',$data['email'])->first();//查询邮箱
+	        //判断邮箱是否注册过
+	        if($first){
+	        	echo 1;
+	        }else{
+	        	$data['password'] = md5($request->input('password'));
+		        $data['money']	  = '0';
+		        $data['addtime']  = time();
+		        $data['ip'] = ip2long($myip);
+		        $res=DB::table('user')->insert($data);
+	        }
+        }
+	}
+
+	public function login(Request $request)
+	{
+		if($request->isMethod('get')){
+	        $where['email']=$request->input('email');
+	        $data = DB::table('user')->where($where)->first();
+	        if($data->password!=md5($request->input('password')))
+	        {
+	        	echo 1;
+	        }else{
+	        	//登陆成功
+	        	$request->session()->put('user'.$data->email,$data);
+	        }
+        }
+	}
+	public function get_client_ip($type = 0,$adv=false) {
+	    $type       =  $type ? 1 : 0;
+	    static $ip  =   NULL;
+	    if ($ip !== NULL) return $ip[$type];
+	    if($adv){//高级模式获取(防止伪装)
+	        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+	            $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+	            $pos    =   array_search('unknown',$arr);
+	            if(false !== $pos) unset($arr[$pos]);
+	            $ip     =   trim($arr[0]);
+	        }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+	            $ip     =   $_SERVER['HTTP_CLIENT_IP'];
+	        }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+	            $ip     =   $_SERVER['REMOTE_ADDR'];
+	        }
+	    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
+	        $ip     =   $_SERVER['REMOTE_ADDR'];
+	    }
+	    // IP地址合法验证
+	    $long = sprintf("%u",ip2long($ip));
+	    $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
+	    return $ip[$type];
+	}
+
+
+	
+    
+}
--- bear/laravel52/resources/views/index/login.php
+++ bear/laravel52/resources/views/index/login.php
@@ -6,10 +6,10 @@
 <link rel="alternate" media="handheld"  />
 <!-- end 云适配 -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>登录-拉勾网-最专业的互联网招聘平台</title>
+<title>虎牙直播</title>
 <meta property="qc:admins" content="23635710066417756375" />
-<meta content="拉勾网是3W旗下的互联网领域垂直招聘网站,互联网职业机会尽在拉勾网" name="description">
-<meta content="拉勾,拉勾网,拉勾招聘,拉钩, 拉钩网 ,互联网招聘,拉勾互联网招聘, 移动互联网招聘, 垂直互联网招聘, 微信招聘, 微博招聘, 拉勾官网, 拉勾百科,跳槽, 高薪职位, 互联网圈子, IT招聘, 职场招聘, 猎头招聘,O2O招聘, LBS招聘, 社交招聘, 校园招聘, 校招,社会招聘,社招" name="keywords">
+<meta content="虎牙网是3W旗下的互联网领域垂直招聘网站,互联网职业机会尽在虎牙网" name="description">
+<meta content="虎牙,虎牙网,虎牙招聘,拉钩, 拉钩网 ,互联网招聘,虎牙互联网招聘, 移动互联网招聘, 垂直互联网招聘, 微信招聘, 微博招聘, 虎牙官网, 虎牙百科,跳槽, 高薪职位, 互联网圈子, IT招聘, 职场招聘, 猎头招聘,O2O招聘, LBS招聘, 社交招聘, 校园招聘, 校招,社会招聘,社招" name="keywords">
 
 <meta name="baidu-site-verification" content="QIQ6KC1oZ6" />
 
@@ -36,32 +36,32 @@ var youdao_conv_id = 271546;
 <body id="login_bg">
 	<div class="login_wrapper">
 		<div class="login_header">
-        	<a href="h/"><img src="images/logo_white.png" width="285" height="62" alt="拉勾招聘" /></a>
+        	<a href="h/"><img src="images/logo_white.png" width="285" height="62" alt="虎牙招聘" /></a>
             <div id="cloud_s"><img src="images/cloud_s.png" width="81" height="52" alt="cloud" /></div>
             <div id="cloud_m"><img src="images/cloud_m.png" width="136" height="95"  alt="cloud" /></div>
         </div>
         
     	<input type="hidden" id="resubmitToken" value="" />		
 		 <div class="login_box">
-        	<form id="loginForm" action="index.html">
-				<input type="text" id="email" name="email" value="" tabindex="1" placeholder="请输入登录邮箱地址" />
+				<input type="text" id="email" name="email" value="" tabindex="1" placeholder="请输入邮箱账号" />
+				<span class="error" style="display:none;" id="ar"></span>
 			  	<input type="password" id="password" name="password" tabindex="2" placeholder="请输入密码" />
-				<span class="error" style="display:none;" id="beError"></span>
-			    <label class="fl" for="remember"><input type="checkbox" id="remember" value="" checked="checked" name="autoLogin" /> 记住我</label>
+				<span class="error" style="display:none;" id="br"></span>
+			    <label class="fl" for="remember"><input type="checkbox" id="remember" value=""  name="autoLogin" /> 7天免登陆</label>
 			    <a href="reset.html" class="fr" target="_blank">忘记密码？</a>
 			    
 				<!--<input type="submit" id="submitLogin" value="登 &nbsp; &nbsp; 录" />-->
-				<a style="color:#fff;" href="index.html" class="submitLogin" title="登 &nbsp; &nbsp; 录"/>登 &nbsp; &nbsp; 录</a>
+				<button style="color:#fff;"  class="submitLogin" title="登 &nbsp; &nbsp; 录">登 &nbsp; &nbsp; 录</button>
 
 			    
 			    <input type="hidden" id="callback" name="callback" value=""/>
                 <input type="hidden" id="authType" name="authType" value=""/>
                 <input type="hidden" id="signature" name="signature" value=""/>
                 <input type="hidden" id="timestamp" name="timestamp" value=""/>
-			</form>
+
 			<div class="login_right">
-				<div>还没有拉勾帐号？</div>
-				<a  href="register.html"  class="registor_now">立即注册</a>
+				<div>还没有虎牙帐号？</div>
+				<a  href="register"  class="registor_now">立即注册</a>
 			    <div class="login_others">使用以下帐号直接登录:</div>
 			    <a  href="h/ologin/auth/sina.html"  target="_blank" class="icon_wb" title="使用新浪微博帐号登录"></a>
 			    <a  href="h/ologin/auth/qq.html"  class="icon_qq" target="_blank" title="使用腾讯QQ帐号登录"></a>
@@ -70,65 +70,51 @@ var youdao_conv_id = 271546;
         <div class="login_box_btm"></div>
     </div>
 
-<script type="text/javascript">
-$(function(){
-	//验证表单
-	 	$("#loginForm").validate({
-	 		/* onkeyup: false,
-	    	focusCleanup:true, */
-	        rules: {
-	    	   	email: {
-	    	    	required: true,
-	    	    	email: true
-	    	   	},
-	    	   	password: {
-	    	    	required: true
-	    	   	}
-	    	},
-	    	messages: {
-	    	   	email: {
-	    	    	required: "请输入登录邮箱地址",
-	    	    	email: "请输入有效的邮箱地址，如：vivi@lagou.com"
-	    	   	},
-	    	   	password: {
-	    	    	required: "请输入密码"
-	    	   	}
-	    	},
-	    	submitHandler:function(form){
-	    		if($('#remember').prop("checked")){
-	      			$('#remember').val(1);
-	      		}else{
-	      			$('#remember').val(null);
-	      		}
-	    		var email = $('#email').val();
-	    		var password = $('#password').val();
-	    		var remember = $('#remember').val();
-	    		
-	    		var callback = $('#callback').val();
-	    		var authType = $('#authType').val();
-	    		var signature = $('#signature').val();
-	    		var timestamp = $('#timestamp').val();
-	    		
-	    		$(form).find(":submit").attr("disabled", true);
-	            $.ajax({
-	            	type:'POST',
-	            	data:{email:email,password:password,autoLogin:remember, callback:callback, authType:authType, signature:signature, timestamp:timestamp},
-	            	url:ctx+'/user/login.json'
-	            }).done(function(result) {
-					if(result.success){
-					 	if(result.content.loginToUrl){
-							window.location.href=result.content.loginToUrl;
-	            		}else{
-	            			window.location.href=ctx+'/';
-	            		} 
-					}else{
-						$('#beError').text(result.msg).show();
-					}
-					$(form).find(":submit").attr("disabled", false);
-	            }); 
-	        }  
-		});
-})
-</script>
+
 </body>
 </html>
+<script type="text/javascript">
+	$('.submitLogin').click(function(){
+    		var email = $('#email').val();
+    		var pwd = $('#password').val();
+    		var remember = $('#remember').is(":checked");
+    		if(remember){
+    			remember = 1;
+    		}else{
+    			remember = 0;
+    		}
+    		var ret = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/;//邮箱正则
+			if(! ret.test(email)){
+				$('#ar').html('邮箱格式错误');
+				$('#ar').attr('style','display:true;')
+				return false;
+			}else{
+				$('#ar').attr('style','display:none;')
+			}
+			if(pwd =='')
+			{
+				$('#br').html('密码不能为空');
+				$('#br').attr('style','display:true;')
+				return false;
+			}else{
+				$('#br').attr('style','display:none;')
+			}
+
+		    $.ajax({
+			   type: "GET",
+			   url: "login_do",
+			   data: "email="+email+"&password="+pwd+"&remember="+remember,
+			   success: function(msg){
+			     if(msg == 1)
+			     {
+			     	$('#ar').html('登录失败了亲');
+					$('#ar').attr('style','display:true;')
+					return false;
+			     }else{
+			     	alert('快去看直播吧');
+			     	location.href = "http://127.0.0.1/laravel521/public/";//location.href实现客户端页面的跳转 
+			     }
+			   }
+			});
+    	})
+</script>
