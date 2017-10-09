b6f818d063e9c68dd6f44f36968154e6b7f04add
小槑 <1744478393@qq.com>
2017-10-09 13:49:21 +0800

    通告
    通告

--- bear/advanced/backend/controllers/OfficialController.php
+++ bear/advanced/backend/controllers/OfficialController.php
@@ -26,12 +26,33 @@ class OfficialController extends Controller
 	//通告列表接口
 	public function actionShow()
 	{
-		$callback=Yii::$app->request()->get('callback');
-		$db=Yii::$app->db;
-		$data=$db->createCommand("select * from official")->queryAll();
-		$msg['data']=$data;
-		$msg['error']=1;
-		echo $callback."(".json_encode($msg).")";
+		$arr = Yii::$app->request->get();
+		$db = Yii::$app->db;
+		$name = Yii::$app->request->get('name') ? Yii::$app->request->get('name') : '';//搜索的条件
+		$page = Yii::$app->request->get('page') ? Yii::$app->request->get('page') : '1';//当前页
+		$num  = 5;//每页显示条数
+		$a    = $db->createCommand("select count(id) from official where content like '%$name%'")->queryAll();
+		$sum  = $a[0]['count(id)'];//总条数
+		$sum_page = ceil($sum/$num);//最大页
+		$limit =($page-1)*$num;//偏移量
+		$msg['prev'] = $page-1<0 ? 1 :$page-1;//上一页
+		$msg['next'] = $page+1 > $sum_page ? $sum_page : $page+1;//下一页
+		$msg['page'] = $page;
+		$msg['end'] = $sum_page;//总页数
+		if (Yii::$app->request->get('id')) {
+			$id=Yii::$app->request->get('id');
+			$res = $db->createCommand("select * from official where id=$id and content like '%$name%' limit $limit,$num ")->queryAll();
+		}else
+		{
+			$res = $db->createCommand("select * from official where content like '%$name%' limit $limit,$num ")->queryAll();
+		}
+		if ($res) {
+			$msg['data']=$res;
+			$msg['error']=1;
+		}else{
+			$msg['error']=0;
+		}
+		return $arr['callback'].'('.json_encode($msg).')';
 	}
 
 }
