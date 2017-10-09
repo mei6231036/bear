d4e7a701310ebd3db7acd32f06655420e131b22a
小槑 <1744478393@qq.com>
2017-10-09 21:04:29 +0800

    低级版的分类展示页面
    更新中..

--- bear/laravel52/app/Http/Controllers/CateController.php
+++ bear/laravel52/app/Http/Controllers/CateController.php
@@ -1,20 +1,57 @@
 <?php 
 namespace App\Http\Controllers;
 use DB;
+use Illuminate\Http\Request;
 class CateController extends Controller
 {
 	public function show()
 	{
-		$cate=DB::table('type')->where('parent_id','=',0)->get();
 		$data=DB::table('type')->get();
-		foreach ($cate as $key => $val) {
+		$data=json_decode(json_encode($data),true);
 			foreach ($data as $k => $v) {
-				if ($val->id==$v->parent_id) {
-					$cate[$key]->child[]=$v;
+				if ($v['parent_id']==0) {
+					$cate[$v['id']]['val']=$v;
+				}else
+				{
+					$cate[$v['parent_id']]['child'][]=$v;
 				}
 			}
-		}
-		return view('cate/cate_list',['cate'=>$cate]);
+		return view('cate/cate_list',['cate'=>$cate,'data'=>$data]);
+	}
+
+	public function listcate(Request $request)
+	{
+		$id=$request->input('id');
+		$data=DB::table('type')->get();
+		$data=json_decode(json_encode($data),true);
+		$da=DB::table('type')->where(['parent_id'=>$id])->get();
+		$da=json_decode(json_encode($da),true);
+			foreach ($data as $k => $v) {
+				if ($v['parent_id']==0) {
+					$cate[$v['id']]['val']=$v;
+				}else
+				{
+					$cate[$v['parent_id']]['child'][]=$v;
+				}
+			}
+		return view('cate/cate_list',['cate'=>$cate,'data'=>$da]);
+	}
+	public function childcate(Request $request)
+	{
+		$id=$request->input('id');
+		$data=DB::table('type')->get();
+		$data=json_decode(json_encode($data),true);
+		$da=DB::table('type')->where(['id'=>$id])->get();
+		$da=json_decode(json_encode($da),true);
+			foreach ($data as $k => $v) {
+				if ($v['parent_id']==0) {
+					$cate[$v['id']]['val']=$v;
+				}else
+				{
+					$cate[$v['parent_id']]['child'][]=$v;
+				}
+			}
+		return view('cate/cate_list',['cate'=>$cate,'data'=>$da]);
 	}
 }
 
