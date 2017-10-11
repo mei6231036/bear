114e5391083977144fc641eb1086ac229e15de96
ymm <1052418826@qq.com>
2017-10-11 20:50:37 +0800

    接口

--- bear/advanced/backend/controllers/CityController.php
+++ bear/advanced/backend/controllers/CityController.php
@@ -8,19 +8,14 @@ class CityController extends Controller
 	//查询某一天各个省注册人数
 	public function actionSel()
 	{
-
 		$callback=Yii::$app->request->get('callback');
 		$month=Yii::$app->request->get('month');
 		$time=strtotime('2017-'.$month.'-24');
-
-
 		$db = Yii::$app->db;
 		//接值
-		// $time=Yii::$app->request->get('time');
-
+		//$time=Yii::$app->request->get('time');
 		//查询省份表
 		$a = $db->createCommand("select * from city ")->queryAll();
-
 		if($time)
 		{
 			$b = $db->createCommand("select count(*) as num,sheng from  user_info inner join user on user_info.user_id = user.user_id where `user`.addtime >= $time GROUP BY sheng")->queryAll();
@@ -34,6 +29,7 @@ class CityController extends Controller
 			if($v['sheng']==$value['id'])
 			{
 				$value['num']=intval($v['num']);
+				
 			}
 		}
 		}
@@ -42,13 +38,17 @@ class CityController extends Controller
 		foreach ($a as $key => $val) {
 			$data['city'][]=$val['city'];
 			$data['num'][]=$val['num'];
+			if ($key==6) {
+					break;
+				}
 
 		}
 		echo $callback."(".json_encode($data).")";
 	}
-	public function actionShow()
+	public function actionLine()
 	{
-		
-		
+		$callback=Yii::$app->request->get('callback');
+		$db = Yii::$app->db;
+
 	}
 }
