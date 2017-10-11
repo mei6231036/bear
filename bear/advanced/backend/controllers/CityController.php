22763e4fb7303abe9d97b38a0a8c5a015e4cd08d
ymm <1052418826@qq.com>
2017-10-11 14:25:58 +0800

    柱形图接口

--- bear/advanced/backend/controllers/CityController.php
+++ bear/advanced/backend/controllers/CityController.php
@@ -10,7 +10,8 @@ class CityController extends Controller
 	{
 		$time = '1507521395';
 		// $data = date('Y-m-d',$time);
-		
+		$callback=Yii::$app->request->get('callback');
+
 		$db = Yii::$app->db;
 		//接值
 		// $time=Yii::$app->request->get('time');
@@ -18,9 +19,19 @@ class CityController extends Controller
 		//查询省份表
 		$a = $db->createCommand("select * from city ")->queryAll();
 		foreach ($a as $key => $value) {
-			 $arr[$value['city']] = $db->createCommand("select count(*) from user inner join user_info on user.user_id = user_info.user_id inner join city on user_info.sheng = city.id where addtime = $time and user_info.sheng = ".$value['id'])->queryOne();;
+			 $arr[$value['city']] = $db->createCommand("select count(*) as num from user inner join user_info on user.user_id = user_info.user_id inner join city on user_info.sheng = city.id where addtime = $time and user_info.sheng = ".$value['id'])->queryOne();;
 		}
-		var_dump($arr);die;
-		var_dump($data);
+		foreach ($arr as $key => $val) {
+			$data['city'][]=$key;
+			foreach ($val as $k => $v) {
+			$data['num'][]=$v;
+			}
+		}
+		echo $callback."(".json_encode($data).")";
+	}
+	public function actionShow()
+	{
+		
+		
 	}
 }
---
+++ bear/advanced/backend/controllers/StatisticaController.php
@@ -0,0 +1,18 @@
+<?php 
+namespace backend\controllers;
+use Yii;
+use yii\web\Controller;
+/**
+* 统计管理
+*/
+class  StatisticaController extends Controller
+{
+	
+	public function actionIndex()
+	{
+		
+	}
+}
+
+
+ ?>
