0e5deafdc7ca7452e5913aadf023d270df558a82
小槑 <1744478393@qq.com>
2017-10-12 18:02:02 +0800

    文件上传
    无刷新分片上传

--- bear/laravel52/app/Http/routes.php
+++ bear/laravel52/app/Http/routes.php
@@ -16,6 +16,8 @@
 Route::any('/home',"home\HomeController@index");
 // 个人中心
 Route::any('/myuser',"MyuserController@index");
+Route::any('/upup',"MyuserController@upup");
+Route::any('/uped',"MyuserController@uped");
 Route::any('/myauth',"MyuserController@myauth");
 Route::any('/myaccount',"MyuserController@myaccount");
 Route::any('/mylevel',"MyuserController@mylevel");
--- bear/laravel52/public/index.php
+++ bear/laravel52/public/index.php
@@ -18,6 +18,17 @@
 | loading any of our classes later on. It feels nice to relax.
 |
 */
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+ini_set('file_uploads','ON');//Http上传文件的开关，默认为开  
+ini_set('max_input_time','90');//通过post,以及put接收数据时间，默认为60秒  
+ini_set('max_execution_time','180');//默认为30秒，脚本执行时间修改为180秒  
+ini_set('post_max_size','10M');//修改post变量由2m变成1om,要比upload_max_filesize大  
+ini_set('upload_max_filesize','8M');//文件上传最大  
+ini_set('memory_limit','90M');//内存使用问题，最好比post_max_size大1.5倍  
 header("content-type:text/html;charset=utf8");
 require __DIR__.'/../bootstrap/autoload.php';
 
